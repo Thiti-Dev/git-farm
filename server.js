bdae30d783952e54e923e90f0a203e3d0c64ad91
initz <thiti2013@gmail.com>
2019-04-25 23:03:20 +0700

    @ALPHA-UNSTABLE 0.00001pk

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+
+// predef
+const app = express();
+const axios = require('axios')
+
+const port = 5000;
+
+// @route       GET /
+// @desc        Show landing default page
+// @access      Public
+// @license     Thiti Mahawannakit 60090500410
+app.get('/', (req,res) => {
+    res.send("This is working");
+});
+
+app.get('/getAPI/:name', (req, res) => {
+    getReposNow(req.params.name);
+});
+
+
+const getRepos = (username) => {
+    try {
+        return axios.get(`https://api.github.com/users/${username}/repos`)
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+const getReposNow = async (username) => {
+    const breeds = getRepos(username)
+        .then(response => {
+            if (response.data) {
+                //console.log(`Got ${Object.entries(response.data).length} breeds`)
+                console.log("Total repos of " + username + " is equal to === " , response.data.length)
+            }
+        })
+        .catch(error => {
+            console.log(error)
+        })
+}
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
